bf16827782d7d5613560b5e78ddd4a0986a2909c
kawanamiyuu <i.am.really.you@gmail.com>
2017-03-09 08:20:10 +0900

    render contents by Vue.js

--- var/www/index.php
+++ var/www/index.php
@@ -1,4 +1,9 @@
 <?php
 
+if (php_sapi_name() === 'cli-server' && strpos($_SERVER['REQUEST_URI'], '/dist/') === 0) {
+    // static files
+    return false;
+}
+
 $context = 'html-app';
 require dirname(dirname(__DIR__)) . '/bootstrap/bootstrap.php';
